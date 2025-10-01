b9b63c1aa306ff3794c1f0fa8f086ae29c5d0851
Topong P. <nong.tickis@gmail.com>
2025-10-01 21:41:08 +0700

    Fix

--- gallery.php
+++ gallery.php
@@ -18,7 +18,7 @@
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
   <div class="container">
-    <a class="navbar-brand fw-bold" href="#">CS Hotel</a>
+    <a class="navbar-brand fw-bold" href="index.php">CS Hotel</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
       <span class="navbar-toggler-icon"></span>
     </button>
--- upload.php
+++ upload.php
@@ -35,7 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
   <div class="container">
-    <a class="navbar-brand fw-bold" href="#">CS Hotel</a>
+    <a class="navbar-brand fw-bold" href="index.php">CS Hotel</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
       <span class="navbar-toggler-icon"></span>
     </button>
